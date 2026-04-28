8dffad6e8a4101e6b2a2033df613dcb4db449038
minhhieu <haminhhieu1005@gmail.com>
2026-04-29 00:14:35 +0700

    change config

--- coral/_version.py
+++ coral/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev73+g8133b6ec3.d20260418'
-__version_tuple__ = version_tuple = (0, 1, 'dev73', 'g8133b6ec3.d20260418')
+__version__ = version = '0.1.dev95+g8d22a4408.d20260428'
+__version_tuple__ = version_tuple = (0, 1, 'dev95', 'g8d22a4408.d20260428')
 
 __commit_id__ = commit_id = None
